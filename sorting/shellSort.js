3eabb477fe5ca8348367d159716e2a60dcdb834a
Ionut Alixandroae <ialixandroae@yahoo.com>
2018-10-02 08:55:47 +0300

    Create shellSort.js

---
+++ sorting/shellSort.js
@@ -0,0 +1,13 @@
+function shellsort(array) {
+  for(var g = 0; g < gaps.length; g++) {
+    var gap = gaps[g];
+    for(var i = gap; i < array.length; i++) {
+      var temp = array[i];
+      for(var j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+        array[j] = array[j - gap];
+      }
+      array[j] = temp;
+    }
+  }
+  return array;
+}
