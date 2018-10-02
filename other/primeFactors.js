188d25152f3af41dfd33e2f77b2c357a73255c2a
Ionut Alixandroae <ialixandroae@yahoo.com>
2018-10-02 09:04:11 +0300

    Create primeFactors.js

---
+++ other/primeFactors.js
@@ -0,0 +1,17 @@
+function primeFactors(n){
+  var factors = [], 
+      divisor = 2;
+  
+  while(n>2){
+    if(n % divisor == 0){
+       factors.push(divisor); 
+       n= n/ divisor;
+    }
+    else{
+      divisor++;
+    }     
+  }
+  return factors;
+}
+
+// primeFactors(69); [3, 23]
