aa61a107e4b724ca214595d4cacb7b5e2cfa0fff
Niezwan <nzwnabdulwahid@gmail.com>
2018-10-02 11:58:25 +0800

    Degree Radian Conversion

---
+++ math/degreeRadianConversion.js
@@ -0,0 +1,16 @@
+// JavaScript implementation of Degree-Radian Conversion
+
+// Author: Niezwan Abdul Wahid
+
+function degreeToRadian(degree) {
+  return degree * (Math.PI / 180);
+}
+
+function radianToDegree(radian) {
+  return radian * (180 / Math.PI);
+}
+
+
+//degreeToRadian(180) === 3.1415926
+
+//radianToDegree(3.1415) === 179.994
