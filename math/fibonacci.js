607f661d4d9eeeae0a90c18e413e12833eab2b60
Nikolas Huebecker <nhuebecker@users.noreply.github.com>
2018-10-01 23:39:30 -0400

    Create fibonacci.js

---
+++ math/fibonacci.js
@@ -0,0 +1,23 @@
+// JavaScript implementation of Fibonacci item finder
+//
+// Author: Nikoals Huebecker
+
+function fibonacci(n){
+  var fib = [0, 1];
+  
+  if (n <= 2) return 1;
+
+  for (var i = 2; i <=n; i++ ){
+   fib[i] = fib[i-1]+fib[i-2];
+  }
+
+ return fib[n];
+} 
+
+// Test
+
+console.log("The first item of the sequence is:")
+console.log(fibonacci(1))
+
+console.log("The tweleth item of the sequence is:")
+console.log(fibonacci(12))
