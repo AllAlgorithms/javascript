fcb87362455989cf4006faae553c1a67e931e288
Aditya Kharote <adityakharote@hotmail.com>
2019-10-01 22:52:09 +0530

    Create anagram.js

---
+++ strings/anagram.js
@@ -0,0 +1,9 @@
+//Check if 2 words are anagrams of each other.
+var o1 = "arms";
+var o2 = "mars"
+//Remove 
+var n1 = o1.replace(/\W+/g,'').toLowerCase().split("").sort().join("");
+var n2 = o2.replace(/\W+/g,'').toLowerCase().split("").sort().join("");
+var isAnagram = n1==n2;
+if(isAnagram) console.log("The two words are anagrams");
+else console.log("The two words are not anagrams");
