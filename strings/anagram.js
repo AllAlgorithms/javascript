da0f3d5a71434971a730a62adc1497fe70eb90b3
Aditya Kharote <adityakharote@hotmail.com>
2019-10-01 22:54:21 +0530

    Fixed incomplete sentence :P

--- strings/anagram.js
+++ strings/anagram.js
@@ -1,7 +1,7 @@
 //Check if 2 words are anagrams of each other.
 var o1 = "arms";
 var o2 = "mars"
-//Remove 
+//Remove non-letter characters, and sort the letters in alphabetical order.
 var n1 = o1.replace(/\W+/g,'').toLowerCase().split("").sort().join("");
 var n2 = o2.replace(/\W+/g,'').toLowerCase().split("").sort().join("");
 var isAnagram = n1==n2;
