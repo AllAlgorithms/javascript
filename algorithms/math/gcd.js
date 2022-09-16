c7e5aab54625314bcba8c840044c6f75fcfd289d
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-09-16 04:14:36 -0400

    Update gcd.js
    Strict mode, better docs, refactoring, etc...

--- algorithms/math/gcd.js
+++ algorithms/math/gcd.js
@@ -1,13 +1,24 @@
-// GCD - greatest common divisor or HCF - highest common factor
+"use strict";
 
-function gcd (small, large) {
-  if (small === 0) { return large } else { return gcd(large % small, small) }
+/** @typedef {(number|bigint)} numeric */
+
+/** @param {numeric} x */
+const abs = x => x < 0 ? -x : x;
+
+/**
+ * GCD - greatest common divisor or HCF - highest common factor.
+ * Using the Euclidean algorithm (not to be confused with Euclid's algo).
+ * @param {numeric} a
+ * @param {numeric} b
+ * returns correct results even for non-integers (sometimes)
+*/
+const gcd = (a, b) => {
+  while (b != 0)
+    [a, b] = [b, a % b];
+  return abs(a)
 }
 
-const gcdList = [[6, 9], [6, 12], [12, 18], [7, 14], [7, 13]]
+const gcdList = [[6, 9], [6, 12], [12, 18], [7, 14], [7, 13]];
 
-for (const set of gcdList) {
-  const small = set[0]
-  const large = set[1]
-  console.log(`GCD for ${small} and ${large} is ${gcd(small, large)}`)
-}
+for (const [small, large] of gcdList)
+  console.log(`GCD for ${small} and ${large} is ${gcd(small, large)}`);
