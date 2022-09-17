32ad85d6720b2c8bb29cd11aa2ba46b09d87b083
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-09-17 11:18:52 -0400

    Update `gcd.js`

--- algorithms/math/gcd.js
+++ algorithms/math/gcd.js
@@ -1,24 +1,27 @@
 "use strict";
 
-/** @typedef {(number|bigint)} numeric */
+/** @typedef {number|bigint} numeric */
 
 /** @param {numeric} x */
 const abs = x => x < 0 ? -x : x;
 
 /**
  * GCD - greatest common divisor or HCF - highest common factor.
- * Using the Euclidean algorithm (not to be confused with Euclid's algo).
+ * Using the Euclidean algorithm (not to be confused with Euclid's algo, which does subtraction).
+ *
+ * Argument order doesn't matter.
+ * `return`s correct results even for non-integers (sometimes, because of rounding errors).
+ * This fn is 2-adic, the variadic implementation is left as an exercise to the reader.
  * @param {numeric} a
  * @param {numeric} b
- * returns correct results even for non-integers (sometimes)
 */
 const gcd = (a, b) => {
-  while (b != 0)
-    [a, b] = [b, a % b];
-  return abs(a)
+    while (b != 0)
+        [a, b] = [b, a % b];
+    return abs(a)
 }
 
-const gcdList = [[6, 9], [6, 12], [12, 18], [7, 14], [7, 13]];
+const tuples = [[6, 9], [6, 12], [-12, -18], [7, 14], [7, 13], [1/2, 2]];
 
-for (const [small, large] of gcdList)
-  console.log(`GCD for ${small} and ${large} is ${gcd(small, large)}`);
+for (const [a, b] of tuples)
+    console.log(`GCD of ${a} and ${b} is ${gcd(a, b)}`);
