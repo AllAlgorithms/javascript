cbdb9580691a34aa32d1e627a46d129a64e66014
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-05 00:23:17 -0400

    Update checkPrime.js

--- algorithms/math/checkPrime.js
+++ algorithms/math/checkPrime.js
@@ -1,25 +1,27 @@
-// JavaScript implementation of prime number checking
-//
-// Author: Nikolas Huebecker
+/**
+ * JS implementation of primality testing
+ * @param {number} n
+ * @author Nikolas Huebecker
+*/
+const isPrime = n => {
+  n = Math.abs(n);
 
-function isPrime(n){
-  var divisor = 2;
+  if (!Number.isInteger(n))
+    return false;
 
-  while (n > divisor){
-    if(n % divisor == 0){
-     return false; 
-    }else {
-      divisor++;
-    }
-  }
+  if (n % 2 === 0)
+    return n === 2;
+
+  const rt = Math.sqrt(n);
+
+  for (let d = 3; d <= rt; d += 2)
+    if (n % d === 0)
+      return false;
+
   return true;
 }
 
 // Test
 
-console.log("The number 137 is prime:"
-console.log(isPrime(137))
-
-
-console.log("The number 16 is prime:"
-console.log(isPrime(16))
+for (const n of [137, 16, 24, 127, 11])
+  console.log(`The number ${n} is prime: ${isPrime(n)}`)
